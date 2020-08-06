b067e8f41ef9d64357adb74f0187116a47f73885
Antonio <antonio241710@gmai.com>
2020-08-06 13:48:16 +0200

    nuestra primera pagina

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'bootstrap/dist/css/bootstrap.css'
+
+import './global.css'
+import BadgeNew from './pages/BagdeNew';
+
+
+// const jsx = <h1>Hello, Platzi Badges</h1>;
+// const element = React.createElement(
+//     'a', 
+//     {href: 'https://platzi.com'}, 
+//     'Ir a Platzi'
+// );
+// const name = 'Stephanie';
+// const sum = () => 3 + 3;
+// const element = React.createElement(
+//     'h1',
+//     {},
+//     `Hola, soy ${name}`
+// );
+// const jsx = <h1>Hello, soy {sum()}</h1>;
+
+// const element = (
+//     <div>
+//         <h1>Hola, soy Richard</h1>
+//         <p>Soy ingeniero frontend.</p>
+//     </div>
+// );
+
+// const element = React.createElement(
+//     'div',
+//     {},
+//     React.createElement('h1', {}, 'Hola, soy Richard'),
+//     React.createElement('p', {}, 'Soy ingeniero de la web.'),
+// );
+
 const container = document.getElementById('app');
 
-const element = <h1>Hello, Platzi Badges</h1>;
-ReactDOM.render(element, container);
+ReactDOM.render(<BadgeNew />, container);
