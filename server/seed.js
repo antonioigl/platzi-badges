9a95e1946bae9574f116da8868e99e51d3567ff0
Antonio <antonio241710@gmail.com>
2020-08-13 12:39:42 +0200

    solicitando datos (GET)

--- server/seed.js
+++ server/seed.js
@@ -3,7 +3,7 @@ const path = require('path');
 const faker = require('faker');
 const md5 = require('md5');
 
-function createBadges(limit = 50) {
+function createBadges(limit = 5) {
   const result = [];
 
   for (let i = 0; i < limit; i++) {
@@ -31,8 +31,8 @@ function main() {
   };
 
   fs.writeFileSync(
-    path.resolve(__dirname, 'db.json'),
-    JSON.stringify(data, null, 4)
+      path.resolve(__dirname, 'db.json'),
+      JSON.stringify(data, null, 4)
   );
 }
 
