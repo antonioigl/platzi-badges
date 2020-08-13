86f0d3c1747859f49dba4063611d2e1fcb97927e
Antonio <antonio241710@gmail.com>
2020-08-13 16:59:53 +0200

    actualizando datos (PUT)

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import {BrowserRouter, Switch ,Route} from "react-router-dom";
 
 import Layout from "./Layout";
-import BadgeNew from "../pages/BadgeNew";
+import Home from "../pages/Home";
 import Badges from "../pages/Badges";
+import BadgeNew from "../pages/BadgeNew";
+import BadgeEdit from "../pages/BadgeEdit";
 import NotFound from "../pages/NotFound";
 
 function App() {
@@ -11,8 +13,10 @@ function App() {
         <BrowserRouter>
             <Layout>
                 <Switch>
+                    <Route exact path="/" component={Home}/>
                     <Route exact path="/badges" component={Badges}/>
                     <Route exact path="/badges/new" component={BadgeNew}/>
+                    <Route exact path="/badges/:badgeId/edit" component={BadgeEdit}/>
                     <Route component={NotFound}/>
                 </Switch>
             </Layout>
